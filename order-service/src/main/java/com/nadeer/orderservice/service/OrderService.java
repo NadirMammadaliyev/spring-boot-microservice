643a434037994c868e9cb422f438d911dc3e2c68
Nadir N. Mammadaliyev <MammadaliyevNN@kapitalho.az>
2023-01-07 23:15:01 +0400

    implementing kafka

--- order-service/src/main/java/com/nadeer/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/nadeer/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package com.nadeer.orderservice.service;
 import com.nadeer.orderservice.dto.InventoryResponse;
 import com.nadeer.orderservice.dto.OrderLineItemsDto;
 import com.nadeer.orderservice.dto.OrderRequest;
+import com.nadeer.orderservice.event.OrderPlacedEvent;
 import com.nadeer.orderservice.model.Order;
 import com.nadeer.orderservice.model.OrderLineItems;
 import com.nadeer.orderservice.repository.OrderRepository;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,6 +28,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         log.info("placeOrder.start orderRequest {}", orderRequest);
@@ -59,6 +62,7 @@ public class OrderService {
 
             if (allProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully!";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
